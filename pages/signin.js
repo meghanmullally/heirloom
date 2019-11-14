eaf88a36d25cfee2a377865a4b1b1f9adc4e90d2
unknown <zamano.jacob@gmail.com>
2019-11-13 18:44:59 -0800

    added fillers for the other js files in the pages so the build will run properly

---
+++ pages/signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from '../components/Head'
+import Navbar from '../components/Navbar';
+import '../styles/index.css'
+
+const Home = () => (
+  <div>
+  <Head></Head>
+  <Navbar></Navbar>
+  <h1>recipes</h1>
+  </div>
+)
+
+export default Home
